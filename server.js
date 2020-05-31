43248711a5898d425b3f3a2157319524d4fd783f
Kleiton Marques <kleitonm.marques@gmail.com>
2020-05-31 13:02:01 -0300

    Criando model de produtos

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const requireDir = require('require-dir')
 
 // Iniciando o App
 const app = express()
 
 // Iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true, useUnifiedTopology: true })
+requireDir('./src/models')
+
+const Product = mongoose.model('Product')
 
 // Primeira rota
 app.get('/', (req, res) => {
-    res.send('Hello Word')
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native'
+    })
+
+    return res.send('Hello Word')
 })
 app.listen(3001)
